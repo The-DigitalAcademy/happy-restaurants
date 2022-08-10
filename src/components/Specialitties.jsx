2db3263415cd5b572b068d0f5d65971f94b10d76
Happy Tlou <happykamogelo33@gmail.com>
2022-08-10 10:27:52 +0200

    added change Specialitties

--- src/components/Specialitties.jsx
+++ src/components/Specialitties.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import styles from '../styles/Specialitties.module.scss'
 import { ReactComponent as Western } from '../assets/food/western.svg'
 import { ReactComponent as Eastern } from '../assets/food/eastern.svg'
-import { ReactComponent as Japan } from '../assets/food/japan.svg'
-import { ReactComponent as Korean } from '../assets/food/korean.svg'
+
 
 const FoodCard=({foodIcon , ctg , description}) => {
     return (
@@ -22,34 +21,23 @@ const Specialitties = () => {
     <div className={styles.container}>
         <div className={styles.title}>
             <h1>Specialitties</h1>
-            <h2>Our Special Cuisine</h2>
+            <h2>Our Special</h2>
         </div>
         <div className={styles.card}>
       <div className='styles.row1'>
           <FoodCard
             foodIcon={<Western />}
-            ctg="Western"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, sunt! Soluta facere est nemo ab modi dignissimos sapiente. At, amet? Quasi, iste? Omnis inventore, nemo perferendis repudiandae excepturi tempore quis!"
+            ctg="Western Food"
+            description="Fried Rice, served with prawn sambal, fried chicken and egg together."
           />
           <FoodCard
             foodIcon={<Eastern />}
-            ctg="Eastern"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, sunt! Soluta facere est nemo ab modi dignissimos sapiente. At, amet? Quasi, iste? Omnis inventore, nemo perferendis repudiandae excepturi tempore quis!"
+            ctg="Eastern Food"
+            description="Chicken, salad with fried rice"
           />
       </div>
 
-      <div className='styles.row2'>
-          <FoodCard
-            foodIcon={<Japan />}
-            ctg="Japan"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, sunt! Soluta facere est nemo ab modi dignissimos sapiente. At, amet? Quasi, iste? Omnis inventore, nemo perferendis repudiandae excepturi tempore quis!"
-          />
-          <FoodCard
-            foodIcon={<Korean />}
-            ctg="Korean"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, sunt! Soluta facere est nemo ab modi dignissimos sapiente. At, amet? Quasi, iste? Omnis inventore, nemo perferendis repudiandae excepturi tempore quis!"
-          />
-      </div>
+
       </div>
     </div>
   )
